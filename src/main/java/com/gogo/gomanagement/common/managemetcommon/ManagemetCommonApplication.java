55683b09a2423565f28762e640e4441c4445a7bd
孙雷 <273662139@qq.com>
2018-08-22 22:31:26 +0800

    test

---
+++ src/main/java/com/gogo/gomanagement/common/managemetcommon/ManagemetCommonApplication.java
@@ -0,0 +1,12 @@
+package com.gogo.gomanagement.common.managemetcommon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ManagemetCommonApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ManagemetCommonApplication.class, args);
+    }
+}
